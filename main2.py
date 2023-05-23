8e983b9d9b68eb33b5fcf2aa4d194aa0260cecaf
prathamesh <prathameshdol196@gmail.com>
2023-05-23 23:56:11 +0530

    initial commit and the code is working and gui in progress

--- main2.py
+++ main2.py
@@ -1,28 +1,66 @@
-import cv2
-import dlib
-import numpy as np
 
-RESIZE_HEIGHT = 360
 
-detector = dlib.get_frontal_face_detector()
+from tkinter import *
+from tkinter import messagebox
 
-cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        continue
-    try:
-        det = max(detector(frame), key=lambda r: r.area())
-    except Exception as e:
-        print(e)
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # Check if the login credentials are valid
+    if username == "admin" and password == "password":
+        messagebox.showinfo("Login Successful", "Welcome, {}".format(username))
+        open_main_window()
     else:
-        cv2.rectangle(
-            frame, (det.left(), det.top()), (det.right(), det.bottom()), (0, 0, 255), 2
-        )
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, "person", font, 0.5, (255, 255, 255), 1)
+        messagebox.showerror("Login Failed", "Invalid username or password")
 
-    cv2.imshow("detect face 0", frame)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+
+def open_main_window():
+    login_window.destroy()
+
+    # Create the main window
+    main_window = Tk()
+    main_window.title("Attendance System")
+    main_window.geometry("200x200")
+
+    # Add your GUI components and functionalities for the main window here
+    start_button = Button(main_window, text="Start Attendance", command=start_attendance)
+
+    main_window.mainloop()
+
+
+def open_signup_window():
+    signup_window = Toplevel(login_window)
+    signup_window.title("Sign Up")
+    signup_window.geometry("200x200")
+
+    # Add your GUI components and functionalities for the signup window here
+
+
+login_window = Tk()
+login_window.title("Login")
+login_window.geometry("400x200")
+login_window.config(padx=50, pady=50, bg="#313552",)
+
+# Username label and entry
+username_label = Label(login_window, text="Username: ", bg="#313552")
+username_label.grid(column=1, row=1)
+username_entry = Entry(login_window, width=42)
+username_entry.grid(column=2, row=1, columnspan=2)
+
+# Password label and entry
+password_label = Label(login_window, text="Password: ", bg="#313552")
+password_label.grid(column=1, row=2)
+password_entry = Entry(login_window, show="*", width=42)
+password_entry.grid(column=2, row=2, columnspan=2)
+
+# Login button
+login_button = Button(login_window, text="Login", command=login)
+login_button.grid(column=3, row=3)
+
+# Signup button
+signup_button = Button(login_window, text="Sign Up", command=open_signup_window)
+signup_button.grid(column=2, row=3)
+
+login_window.mainloop()
--- main3.py
+++ main3.py
@@ -7,6 +7,9 @@ import time
 import numpy as np
 import datetime
 import pandas as pd
+from tkinter import *
+from tkinter import messagebox
+import random
 
 
 def known_student_names():
@@ -62,55 +65,171 @@ def attendance(present_list):
     df.to_csv('attendance.csv', index=False)
 
 
-known_face_names = known_student_names()
-print("known_face_names")
-known_face_encodings = known_student_face_encodings()
-print("known_face_encodings")
+def start_attendance():
+    known_face_names = known_student_names()
+    print("known_face_names")
+    known_face_encodings = known_student_face_encodings()
+    print("known_face_encodings")
 
-video_capture = cv2.VideoCapture(0)  # capture video
-present_list = []
-start_time = time.time()
-while time.time() - start_time < 6:  # 600 seconds = 10 minutes:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
+    video_capture = cv2.VideoCapture(0)  # capture video
+    present_list = []
+    start_time = time.time()
+    while time.time() - start_time < 6:  # 600 seconds = 10 minutes:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    faces_in_frame = face_recognition.face_locations(small_frame)  # face locations
-    faces_in_frame_encoding = face_recognition.face_encodings(small_frame, faces_in_frame)  # encoding
+        faces_in_frame = face_recognition.face_locations(small_frame)  # face locations
+        faces_in_frame_encoding = face_recognition.face_encodings(small_frame, faces_in_frame)  # encoding
 
-    for encode, faceloc in zip(faces_in_frame_encoding, faces_in_frame):
-        matches = face_recognition.compare_faces(known_face_encodings, encode)
-        face_dis = face_recognition.face_distance(known_face_encodings, encode)
-        matchIndex = np.argmin(face_dis)
-        # name = "unknown"
-        print("in for encode, faceloc in zip(faces_in_frame_encoding, faces_in_frame) ")
+        for encode, faceloc in zip(faces_in_frame_encoding, faces_in_frame):
+            matches = face_recognition.compare_faces(known_face_encodings, encode)
+            face_dis = face_recognition.face_distance(known_face_encodings, encode)
+            matchIndex = np.argmin(face_dis)
+            # name = "unknown"
+            print("in for encode, faceloc in zip(faces_in_frame_encoding, faces_in_frame) ")
 
-        if matches[matchIndex]:
-            name = known_face_names[matchIndex].title()
-            print("attendance")
-            y1, x2, y2, x1 = faceloc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            if matches[matchIndex]:
+                name = known_face_names[matchIndex].title()
+                print("attendance")
+                y1, x2, y2, x1 = faceloc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
 
-            # draw a box around face
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-            cv2.rectangle(frame, (x1, y2 - 25), (x2, y2), (0, 0, 255), cv2.FILLED)
+                # draw a box around face
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                cv2.rectangle(frame, (x1, y2 - 25), (x2, y2), (0, 0, 255), cv2.FILLED)
 
-            # Draw a label with a name below the face
-            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
-            if name not in present_list:
-                present_list.append(name)
+                # Draw a label with a name below the face
+                cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                if name not in present_list:
+                    present_list.append(name)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
+        # Display the resulting image
+        cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-attendance(present_list)
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    attendance(present_list)
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+def login():
+    username = username_entry.get()
+    password = password_entry.get()
+    college = college_entry.get()
+
+
+    # Check if the login credentials are valid
+    with open(file="teachers/data.json") as file:
+        pass
+
+    if username == "admin" and password == "password":
+        messagebox.showinfo("Login Successful", "Welcome, {}".format(username))
+        open_main_window()
+
+    else:
+        messagebox.showerror("Login Failed", "Invalid username or password")
+
+
+def open_main_window():
+    login_window.destroy()
+
+    # Create the main window
+    main_window = Tk()
+    main_window.title("Attendance System")
+    main_window.geometry("400x200")
+
+    # GUI components and functionalities for the main window
+    start_button = Button(main_window, text="Start Attendance", command=start_attendance)
+    start_button.grid(column=1, row=1)
+
+    main_window.mainloop()
+
+
+def open_signup_window():
+    login_window.destroy()
+
+    def signup():
+        email = signup_email_entry.get()
+        password = signup_email_entry.get()
+        name = name_entry.get()
+        college = college_entry.get()
+
+
+    # Create the signup window
+    signup_window = Tk()
+    signup_window.title("Sign Up")
+    signup_window.geometry("400x200")
+    signup_window.config(padx=50, pady=50, bg="#313552", )
+
+    # GUI components and functionalities for the signup window
+
+    # Username label and entry
+    signup_email_label = Label(signup_window, text="Username: ", bg="#313552")
+    signup_email_label.grid(column=1, row=1)
+    signup_email_entry = Entry(signup_window, width=42)
+    signup_email_entry.grid(column=2, row=1, columnspan=2)
+
+    # Password label and entry
+    password_label = Label(signup_window, text="Email: ", bg="#313552")
+    password_label.grid(column=1, row=2)
+    signup_password_entry = Entry(signup_window, width=42)
+    signup_password_entry.grid(column=2, row=2, columnspan=2)
+
+    # College label and entry
+    college_label = Label(signup_window, text="College: ", bg="#313552")
+    college_label.grid(column=1, row=3)
+    college_entry = Entry(signup_window, width=42)
+    college_entry.grid(column=2, row=3, columnspan=2)
+
+    # Name label and entry
+    name_label = Label(signup_window, text="Name: ", bg="#313552")
+    name_label.grid(column=1, row=4)
+    name_entry = Entry(signup_window, width=42)
+    name_entry.grid(column=2, row=4, columnspan=2)
+
+    # Signup button
+    signup_button = Button(signup_window, text="Sign Up", command=signup)
+    signup_button.grid(column=2, row=5)
+
+
+login_window = Tk()
+login_window.title("Login")
+login_window.geometry("400x200")
+login_window.config(padx=50, pady=50, bg="#313552",)
+
+# Username label and entry
+username_label = Label(login_window, text="Username: ", bg="#313552")
+username_label.grid(column=1, row=1)
+username_entry = Entry(login_window, width=42)
+username_entry.grid(column=2, row=1, columnspan=2)
+
+# Password label and entry
+password_label = Label(login_window, text="Password: ", bg="#313552")
+password_label.grid(column=1, row=2)
+password_entry = Entry(login_window, show="*", width=42)
+password_entry.grid(column=2, row=2, columnspan=2)
+
+# Password label and entry
+college_label = Label(login_window, text="College: ", bg="#313552")
+college_label.grid(column=1, row=3)
+college_entry = Entry(login_window, width=42)
+college_entry.grid(column=2, row=3, columnspan=2)
+
+# Login button
+login_button = Button(login_window, text="Login", command=login)
+login_button.grid(column=3, row=4)
+
+# Signup button
+signup_button = Button(login_window, text="Sign Up", command=open_signup_window)
+signup_button.grid(column=2, row=4)
+
+login_window.mainloop()
+
 
